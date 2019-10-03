6bee36f7e77f23c55206d2ae7f84057af15ebd4c
Dan Nicholson <dan.nicholson@gmail.com>
2019-10-03 21:14:25 +0100

    Blink handling and list management

--- modules/pixels.py
+++ modules/pixels.py
@@ -1,9 +1,11 @@
 # sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 
 import neopixel
-
+from time import sleep
 
 class NeoPixel:
+    COLOUR_OFF = (0, 0, 0)
+
     def __init__(self, pin, count):
         self.pin = pin
         self.count = count
@@ -12,7 +14,7 @@ class NeoPixel:
     def __del__(self):
         i = 0
         while i < self.count:
-            self.pixels[i] = (0, 0, 0)
+            self.pixels[i] = NeoPixel.COLOUR_OFF
 
     def set(self, number, color):
         """
@@ -23,7 +25,19 @@ class NeoPixel:
         :param number: pixel number (starting from 0)
         :param color: (R, G, B)
         """
-        self.pixels[number] = color
+        if type(number) is list:
+            for n in number:
+                self.pixels[n] = color
+        else:
+            self.pixels[number] = color
 
     def fill(self, color):
         self.pixels.fill(color)
+
+    def blink(self, number, color, count=1):
+        self.set(number, color)
+        for i in range(count-1):
+            sleep(0.5)
+            self.set(number, NeoPixel.COLOUR_OFF)
+            sleep(0.5)
+            self.set(number, color)
