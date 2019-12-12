46d9e62f2f2032b47f5aae8e59dda3c4fefa7da1
Dan Nicholson <dan.nicholson@gmail.com>
2019-12-12 22:37:29 +0000

    9 LEDs

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -6,7 +6,9 @@ Servo lowerNeck;
 Servo upperNeck;
 Servo rotate;
 
-CRGB leds[5];
+#define LED_COUNT 9
+
+CRGB leds[LED_COUNT];
  
 void setup() 
 { 
@@ -19,19 +21,22 @@ void setup()
   upperNeck.write(100);
   rotate.write(90);
   
-  FastLED.addLeds<WS2812, 13, GRB>(leds, 5);
+  FastLED.addLeds<WS2812, 13, GRB>(leds, LED_COUNT);
   
-  for (int i = 0; i < 5; i++){
+  for (int i = 0; i < LED_COUNT; i++){
     
-    leds[i] = CRGB(0,0,255);
+    leds[i] = CRGB(0,0,5);
   }
   FastLED.show();
   delay(1000);
-  for (int i = 0; i < 5; i++){
+  for (int i = 0; i < LED_COUNT; i++){
     
     leds[i] = CRGB(0,0,0);
   }
-  leds[1] = CRGB(0,50,0);
+  for (int i = 5; i < LED_COUNT; i++){
+    leds[i] = CRGB(5,5,5);
+  }
+  leds[1] = CRGB(0,5,0);
   FastLED.show();
 } 
  
@@ -46,11 +51,11 @@ void loop()
     int val2 = Serial.readStringUntil(',').toInt();
     Serial.read();
     int rotation = Serial.readString().toInt();
-//    Serial.print(val1);
-//    Serial.print('.');
-//    Serial.print(val2);
-//    Serial.print('.');
-//    Serial.println(rotation);
+    Serial.print(val1);
+    Serial.print('.');
+    Serial.print(val2);
+    Serial.print('.');
+    Serial.println(rotation);
     lowerNeck.write(val1);
     upperNeck.write(val2);
     rotate.write(rotation);
