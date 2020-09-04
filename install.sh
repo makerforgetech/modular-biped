ece7349c6f0dea0e843a7c34267c48f280610720
Dan Nicholson <dan.nicholson@gmail.com>
2020-09-04 18:16:31 +0100

    Update dependencies

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 
 # INSTALL DEPENDENCIES FOR SPEECH RECOGNITION
+sudo apt-get install python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev
 sudo apt-get install libpulse-dev
 apt-get install swig
 python3 -m pip install --upgrade pip setuptools wheel
