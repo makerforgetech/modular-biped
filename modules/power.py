af83e0e4f6a2edc44ad1325ec15c5ad7dbab61b3
Dan Nicholson <dan.nicholson@gmail.com>
2021-04-07 21:36:38 +0100

    Disable relay off state to debug power issue

--- modules/power.py
+++ modules/power.py
@@ -3,6 +3,8 @@ from pubsub import pub
 from modules.arduinoserial import ArduinoSerial
 
 class Power:
+    STATE_ON = 0
+    STATE_OFF = 0 #1 - hijacked to stay on all the time to test power bug
     def __init__(self, pin, **kwargs):
         self.pin = pin
         self.active_count = 0
@@ -11,18 +13,18 @@ class Power:
         pub.subscribe(self.use, 'power:use')
         pub.subscribe(self.release, 'power:release')
         pub.subscribe(self.exit, 'power:exit')
-        pub.sendMessage('serial', type=ArduinoSerial.DEVICE_PIN, identifier=self.pin, message=1)  # high is off, low is on
+        pub.sendMessage('serial', type=ArduinoSerial.DEVICE_PIN, identifier=self.pin, message=Power.STATE_OFF)  # high is off, low is on
 
     def __del__(self):
         if self.timer is not None:
             self.timer.cancel()
 
     def exit(self):
-        pub.sendMessage('serial', type=ArduinoSerial.DEVICE_PIN, identifier=self.pin, message=1)
+        pub.sendMessage('serial', type=ArduinoSerial.DEVICE_PIN, identifier=self.pin, message=Power.STATE_OFF)
 
     def use(self):
         self.active_count = self.active_count + 1
-        pub.sendMessage('serial', type=ArduinoSerial.DEVICE_PIN, identifier=self.pin, message=0)
+        pub.sendMessage('serial', type=ArduinoSerial.DEVICE_PIN, identifier=self.pin, message=Power.STATE_ON)
         if self.timer is not None:
             self.timer.cancel()
 
@@ -30,7 +32,7 @@ class Power:
         self.active_count = self.active_count - 1
         if self.active_count <= 0:
             self.active_count = 0  # just ensure that it hasn't gone below 0
-            pub.sendMessage('serial', type=ArduinoSerial.DEVICE_PIN, identifier=self.pin, message=1)
+            pub.sendMessage('serial', type=ArduinoSerial.DEVICE_PIN, identifier=self.pin, message=Power.STATE_OFF)
             if self.thread:
                 if self.timer is not None:
                     self.timer.cancel()
@@ -42,4 +44,4 @@ class Power:
     def _off(self):
         if self.active_count <= 0:
             print('off')
-            pub.sendMessage('serial', type=ArduinoSerial.DEVICE_PIN, identifier=self.pin, message=1)
+            pub.sendMessage('serial', type=ArduinoSerial.DEVICE_PIN, identifier=self.pin, message=Power.STATE_OFF)
