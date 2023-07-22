075f78fbc0056f0f3733c9b41ac93c9b4ba58ef0
Emir HAMURCU <134494889+SentryCoderDev@users.noreply.github.com>
2023-07-22 22:38:58 +0300

    manual_startup.sh Güncellendi

--- manual_startup.sh
+++ manual_startup.sh
@@ -1,4 +1,6 @@
 sudo pkill -f /home/archie/companion-robot/main.py
+sudo pkill -f /home/archie/companion-robot/RFID.py
 sudo modprobe bcm2835-v4l2 # Enable camera
 sudo pigpiod # GPIO @todo may not be used
 sudo python3 /home/archie/companion-robot/main.py manual
+sudo python3 /home/archie/companion-robot/RFID.py
