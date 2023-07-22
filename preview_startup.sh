7482e43e9a0071c44b40fd906afc68b4dd9bcea1
Emir HAMURCU <134494889+SentryCoderDev@users.noreply.github.com>
2023-07-22 23:00:14 +0300

    preview_startup.sh Güncellendi

--- preview_startup.sh
+++ preview_startup.sh
@@ -2,5 +2,19 @@ sudo pkill -f /home/archie/companion-robot/main.py
 sudo pkill -f /home/archie/companion-robot/RFID.py
 sudo modprobe bcm2835-v4l2 # Enable camera
 sudo pigpiod # GPIO @todo may not be used
-sudo python3 /home/archie/companion-robot/main.py preview
 sudo python2 /home/archie/companion-robot/RFID.py
+
+#!/bin/bash
+# Define the username as a variable (replace "archie" with the actual username)
+username="archie"
+
+# Check if the user is logged in
+if who | grep -wq "$username"; then
+  # If the user is logged in, start the desired file here
+  sudo python3 /home/archie/companion-robot/main.py preview
+  echo "User is logged in, running the file..."
+  /path/to/yourfile.sh
+else
+  # If the user is not logged in, print a message
+  echo "User is not logged in, the file won't be executed."
+fi
