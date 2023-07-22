9a6da7537a47fc4eea5a8a671ed7ff3de933d00b
Emir HAMURCU <134494889+SentryCoderDev@users.noreply.github.com>
2023-07-22 23:01:21 +0300

    preview_startup.sh Güncellendi

--- preview_startup.sh
+++ preview_startup.sh
@@ -11,9 +11,8 @@ username="archie"
 # Check if the user is logged in
 if who | grep -wq "$username"; then
   # If the user is logged in, start the desired file here
-  sudo python3 /home/archie/companion-robot/main.py preview
   echo "User is logged in, running the file..."
-  /path/to/yourfile.sh
+  sudo python3 /home/archie/companion-robot/main.py preview
 else
   # If the user is not logged in, print a message
   echo "User is not logged in, the file won't be executed."
