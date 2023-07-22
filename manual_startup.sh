fd508bd5ebc315cf8232f0698e2f113f924626f5
Emir HAMURCU <134494889+SentryCoderDev@users.noreply.github.com>
2023-07-22 23:02:25 +0300

    manual_startup.sh Güncellendi

--- manual_startup.sh
+++ manual_startup.sh
@@ -2,5 +2,18 @@ sudo pkill -f /home/archie/companion-robot/main.py
 sudo pkill -f /home/archie/companion-robot/RFID.py
 sudo modprobe bcm2835-v4l2 # Enable camera
 sudo pigpiod # GPIO @todo may not be used
-sudo python3 /home/archie/companion-robot/main.py manual
 sudo python3 /home/archie/companion-robot/RFID.py
+
+#!/bin/bash
+# Define the username as a variable (replace "archie" with the actual username)
+username="archie"
+
+# Check if the user is logged in
+if who | grep -wq "$username"; then
+  # If the user is logged in, start the desired file here
+  echo "User is logged in, running the file..."
+  sudo python3 /home/archie/companion-robot/main.py manual
+else
+  # If the user is not logged in, print a message
+  echo "User is not logged in, the file won't be executed."
+fi
