eb2b51a774a3dbd7d7b1f4c704d865469370e44b
Emir HAMURCU <134494889+SentryCoderDev@users.noreply.github.com>
2023-07-22 22:36:21 +0300

    startup.sh Güncellendi

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 sudo pkill -f /home/archie/companion-robot/main.py
+sudo pkill -f /home/archie/companion-robot/RFID.py
 sudo modprobe bcm2835-v4l2 # Enable camera
 sudo pigpiod # GPIO @todo may not be used
 sudo python3 /home/archie/companion-robot/main.py
+sudo python3 /home/archie/companion-robot/RFID.py
 
