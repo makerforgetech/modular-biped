682c48d2cd3621c1b95e387bc65de09d8b092aed
Emir HAMURCU <134494889+SentryCoderDev@users.noreply.github.com>
2023-09-17 09:47:12 +0300

    chatbot.py Güncellendi

--- modules/chatbot/chatbot.py
+++ modules/chatbot/chatbot.py
@@ -1,8 +1,5 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-import speech_recognition as sr
-from gtts import gTTS
-import os
 import logging
 
 logger = logging.getLogger()
@@ -12,51 +9,17 @@ class MyChatBot:
     
     def __init__(self, **kwargs):
         self.chatbot = ChatBot('Robot')
-        #chatbot için yeni bir eğitim oluşturun
+        # Create a new trainer for the chatbot
         self.trainer = ChatterBotCorpusTrainer(self.chatbot)
 
-        self.trainer.train("chatterbot.corpus.turkish")
-        # Chatbot'u Türkçe bütüncesine"(corpusuna)"göre eğitin
-        self.trainer.train("chatterbot.corpus.turkish.greetings")
-        # Chatbot'u Türkçe Selamlama bütüncesine"(corpusuna)"göre eğitin
-        self.trainer.train("chatterbot.corpus.turkish.conversations")
-        # Chatbot'u Türkçe Konuşma bütüncesine"(corpusuna)"göre eğitin
+        # Train the chatbot based on the english corpus
+        self.trainer.train("chatterbot.corpus.english")
 
-        self.recognizer = sr.Recognizer()
+        # Train based on english greetings corpus
+        self.trainer.train("chatterbot.corpus.english.greetings")
 
-    def get_audio_input(self):
-        with sr.Microphone() as source:
-            print("Dinliyorum...")
-            audio = self.recognizer.listen(source)
+        # Train based on the english conversations corpus
+        self.trainer.train("chatterbot.corpus.english.conversations")
 
-        try:
-            text = self.recognizer.recognize_google(audio, language='tr-TR')
-            print("Ses Algılandı:", text)
-            return text
-        except sr.UnknownValueError:
-            print("Ses anlaşılamadı")
-            return ""
-        except sr.RequestError:
-            print("Google Speech Recognition servisine şu anda ulaşılamıyor lütfen daha sonra tekrar deneyiniz")
-            return ""
-
-    def speak(self, text):
-        tts = gTTS(text=text, lang='tr')
-        tts.save("output.mp3")
-        os.system("mpg321 output.mp3")
-        print("ChatBot:", text)
-
-    def chat(self):
-        while True:
-            user_input = self.get_audio_input()
-
-            if user_input.lower() == "Konuşma motorunu kapat":
-                self.speak("Konuşma motoru kapatılıyor!")
-                break
-
-            response = self.chatbot.get_response(user_input)
-            self.speak(response.text)
-
-if __name__ == "__main__":
-    chatbot = MyChatBot()
-    chatbot.chat()
+    def get_response(self, text):
+        return self.chatbot.get_response(text)
