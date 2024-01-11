165cfe6ba66f79926b39be89e8f83cceda316854
danic85 <dan.nicholson@outlook.com>
2024-01-11 20:49:45 +0000

    Test viam logging

--- modules/logwrapper.py
+++ modules/logwrapper.py
@@ -1,9 +1,11 @@
 import logging
 from pubsub import pub
 import os
+from viam.logging import getLogger
+LOGGER = getLogger(__name__)
+LOGGER.debug('INIT MAKERFORGE LOGGER')
 
 class LogWrapper:
-
     levels = ['notset', 'debug', 'info', 'warning', 'error', 'critical']
 
     def __init__(self, **kwargs):
@@ -25,9 +27,23 @@ class LogWrapper:
             os.rename(self.file, self.file + '.previous')
 
     def log(self, type, msg):
+        msg = '[LOGGING] ' + msg
         # Translate type string to log level (0 - 50)
         logging.log(LogWrapper.levels.index(type)*10, msg)
         # if type == 'error' or type == 'warning':
         if self.translator is not None:
             msg = self.translator.request(msg)
         print('LogWrapper: ' + type + ' - ' + str(msg))
+        self.log_viam(type, msg)
+        
+    def log_viam(self, type, msg):
+        if type == 'debug':
+            LOGGER.debug(msg)
+        elif type == 'info':
+            LOGGER.info(msg)
+        elif type == 'warning':
+            LOGGER.warn(msg)
+        elif type == 'error':
+            LOGGER.error(msg)
+        elif type == 'critical':
+            LOGGER.critical(msg)
