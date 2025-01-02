ea6d606f572319265e1d6fc32550b7af406c9b8c
danic85 <dan.nicholson@outlook.com>
2025-01-02 21:58:00 +0000

    Clean up

--- modules/i2c.py
+++ modules/i2c.py
@@ -4,17 +4,14 @@ from time import sleep
 
 from adafruit_servokit import ServoKit
 
-
 class I2C:
     def __init__(self, **kwargs):
         # Scan with `sudo i2cdetect -y 1`
-        kit = ServoKit(channels=16)
+        self.servos = ServoKit(channels=16)
         # https://learn.adafruit.com/adafruit-16-channel-servo-driver-with-raspberry-pi/using-the-adafruit-library
-        print("I2C initialized, moving servo to 180")
-        kit.servo[0].angle = 180 # Range 0 to 180
+        print("Initiated i2c moving servo to 90")
+        self.moveServo(0, 90)
         sleep(1)
-        print("moving servo to 90")
-        kit.servo[0].angle = 90 # Range 0 to 180
-        sleep(1)
-        print("moving servo to 0")
-        kit.servo[0].angle = 0 # Range 0 to 180
+    
+    def moveServo(self, index, angle):
+        self.servos.servo[index].angle = angle
